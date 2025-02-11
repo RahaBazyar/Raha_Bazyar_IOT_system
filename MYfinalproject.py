c3a6b6a74906666d11c16cbfc3652d007634a9ec
RahaBazyar <bazyarr86@gmail.com>
2025-02-11 11:50:59 +0330

    Update MYfinalproject.py

--- MYfinalproject.py
+++ MYfinalproject.py
@@ -1,2 +1,24 @@
 
 
+
+
+import numpy as np
+
+class Device:  
+    def __init__(self, topic):  
+        self.topic = topic  
+        self.topic_list = topic.split('/')      
+        self.location = self.topic_list[0] 
+        self.group = self.topic_list[1]     
+        self.device_type = self.topic_list[2]   
+        self.name = self.topic_list[3]     
+        print(f'Device {self.name} initialized with topic {self.topic}')  
+
+a1 = Device(topic='home/parking/lamps/lamp9')  
+
+print(f'Topic: {a1.topic}')  
+print(f'Name: {a1.name}')  
+print(f'Device Type: {a1.device_type}')  
+print(f'Group: {a1.group}')  
+print(f'Location: {a1.location}')
+
