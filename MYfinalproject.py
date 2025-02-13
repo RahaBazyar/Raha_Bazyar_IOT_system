85b4eea05e910f43701c0a96f25b55922381befe
RahaBazyar <bazyarr86@gmail.com>
2025-02-13 12:16:52 +0330

    Update MYfinalproject.py

--- MYfinalproject.py
+++ MYfinalproject.py
@@ -1,8 +1,11 @@
+'''
 
 
+'''
 import numpy as np
 
 class Device:  
+   
     def __init__(self, topic):  
         self.topic = topic  
         self.topic_list = topic.split('/')      
@@ -10,13 +13,35 @@ class Device:
         self.group = self.topic_list[1]     
         self.device_type = self.topic_list[2]   
         self.name = self.topic_list[3]     
-        print(f'Device {self.name} initialized with topic {self.topic}')  
+        print(f'Device {self.name} initialized with topic {self.topic}')
+        
+    def turn_on(self):
+        self.status = 'on'
+        print(f'Device {self.name} turned ON')  
+
+    def turn_off(self):  
+        self.status = 'off'  
+        print(f'Device {self.name} turned OFF')  
+                
+    def get_status(self): 
+        print(f'{Device.name} status is {Device.get_status()}')
+        return self.status  
 
 a1 = Device(topic='home/parking/lamps/lamp9')  
-
 print(f'Topic: {a1.topic}')  
 print(f'Name: {a1.name}')  
 print(f'Device Type: {a1.device_type}')  
 print(f'Group: {a1.group}')  
 print(f'Location: {a1.location}')
+    
+class Sensor:  
+    def __init__(self, name, group, unit, pin):  
+        self.name = name  
+        self.group = group  
+        self.unit = unit  
+        self.pin = pin  
+        self.current_value = None
+        
+    def read_sensor(self):
+        return np.random.uniform(20,25)
 
